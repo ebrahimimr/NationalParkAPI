b47f352e8a91f5b9bc6ccd8d6a0d5ee0c08dace0
MohammadReza Ebrahimi <ebrahimi.mr@gmail.com>
2022-04-19 14:01:36 +0430

    What Automapper Does

--- ParkyAPI/Controllers/NationalParksController.cs
+++ ParkyAPI/Controllers/NationalParksController.cs
@@ -73,7 +73,19 @@ namespace ParkyAPI.Controllers
             {
                 return NotFound();
             }
-            return Ok(_mapper.Map<NationalParkDto>(obj));
+
+            var objDto = _mapper.Map<NationalParkDto>(obj);
+            
+            //Automapper Do This
+            //var objDto = new NationalParkDto()
+            //{
+            //    Created=obj.Created,
+            //    Id=obj.Id,
+            //    Name=obj.Name,
+            //    State= obj.State
+            //};
+
+            return Ok(objDto);
         }
 
         [HttpPost]
