d090de0376c713e2d28f7c6a7ad2419535ce345b
MohammadReza Ebrahimi <ebrahimi.mr@gmail.com>
2022-04-21 13:31:38 +0430

    End Of Parky Web (8)

---
+++ ParkyWeb/SD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyWeb
+{
+    public class SD
+    {
+        public static string APIBaseUrl = "https://localhost:44346/"; 
+        public static string NationalParkAPIPath = "api/v1/nationalpark";
+        public static string TrailAPIPath = "api/v1/trails";
+    }
+}
